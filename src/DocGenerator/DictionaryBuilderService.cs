a072b494c4affdda24800211dc9db8cab079a4ff
doricat <doricat@outlook.com>
2022-05-02 00:45:14 +0800

    实现生成结果主方法

--- src/DocGenerator/DictionaryBuilderService.cs
+++ src/DocGenerator/DictionaryBuilderService.cs
@@ -1,5 +1,9 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.Composition;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.VisualStudio.Shell;
 
 namespace DocGenerator
 {
@@ -8,7 +12,39 @@ namespace DocGenerator
     {
         public IDictionary<string, object> GenerateDictionary(SyntaxContext context)
         {
-            throw new System.NotImplementedException();
+            var result = new Dictionary<string, object>();
+            var syntax = context.Syntax;
+            var semanticModel = ThreadHelper.JoinableTaskFactory.Run(() => context.Document.GetSemanticModelAsync());
+
+            switch (syntax)
+            {
+                case ClassDeclarationSyntax classDeclarationSyntax:
+                    ParseClassDeclarationSyntax(classDeclarationSyntax, semanticModel, result);
+                    break;
+                case IdentifierNameSyntax identifierNameSyntax:
+                    ParseIdentifierNameSyntax(identifierNameSyntax, semanticModel, result);
+                    break;
+                case GenericNameSyntax genericNameSyntax:
+                    ParseGenericNameSyntax(genericNameSyntax, semanticModel, result);
+                    break;
+            }
+
+            return result;
+        }
+
+        protected void ParseClassDeclarationSyntax(ClassDeclarationSyntax syntax, SemanticModel semanticModel, IDictionary<string, object> result)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void ParseIdentifierNameSyntax(IdentifierNameSyntax syntax, SemanticModel semanticModel, IDictionary<string, object> result)
+        {
+            throw new NotImplementedException();
+        }
+
+        protected void ParseGenericNameSyntax(GenericNameSyntax syntax, SemanticModel semanticModel, IDictionary<string, object> result)
+        {
+            throw new NotImplementedException();
         }
     }
 }
