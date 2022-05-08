fbdef499017a4d82583666bef3d49c13bd8efc27
doricat <doricat@outlook.com>
2022-05-08 20:55:26 +0800

    删除多余文件

--- src/DocGenerator/IDictionaryBuilderService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-
-namespace DocGenerator
-{
-    public interface IDictionaryBuilderService
-    {
-        IDictionary<string, object> GenerateDictionary(SyntaxContext context);
-    }
-}
